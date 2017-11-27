f544d7d3f250f0116cf72f05b089f3444d093047
xkang-labu <676613599@qq.com>
2017-11-27 21:53:20 +0800

    25

---
+++ src/problem_25.java
@@ -0,0 +1,21 @@
+/**
+ * Created by kangkang on 2017/10/19
+ */
+//class TreeNode {
+//    public int value;
+//    public TreeNode left;
+//    public TreeNode right;
+//
+//    TreeNode(int value) {
+//        this.value = value;
+//    }
+//}
+public class problem_25 {
+    public static void deepsearch(TreeNode root) {
+        if (root == null)
+            return;
+        if (root.left != null) {
+
+        }
+    }
+}

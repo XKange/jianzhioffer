2a02918bc86a6d29cb8e090b72cfadb6097071ef
xiangkang <676613599@qq.com>
2017-12-16 22:34:36 +0800

    update 17

--- src/problem_17.java
+++ src/problem_17.java
@@ -3,4 +3,27 @@
  * Description:  输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
  */
 public class problem_17 {
+    public boolean HasSubtree(TreeNode root1,TreeNode root2) {
+        boolean result = false;
+
+        // 这个递归逻辑最关键
+        if (root1 != null && root2 != null) {
+            if (root1.val == root2.val)
+                result = dosTree1HaveTree2(root1, root2);
+            if (!result)
+                result = HasSubtree(root1.left, root2);
+            if (!result)
+                result = HasSubtree(root1.right, root2);
+        }
+        return result;
+    }
+    public boolean dosTree1HaveTree2(TreeNode root1, TreeNode root2) {
+        if (root2 == null)
+            return true;
+        if (root1 == null)
+            return false;
+        if (root1.val != root2.val)
+            return false;
+        return dosTree1HaveTree2(root1.left, root2.left) && dosTree1HaveTree2(root1.right, root2.right);
+    }
 }
--- src/problem_18.java
+++ src/problem_18.java
@@ -3,27 +3,4 @@
  * Description:  操作给定的二叉树，将其变换为源二叉树的镜像。
  */
 public class problem_18 {
-    public boolean HasSubtree(TreeNode root1,TreeNode root2) {
-        boolean result = false;
-
-        // 这个递归逻辑最关键
-  ·      if (root1 != null && root2 != null) {
-            if (root1.val == root2.val)
-                result = dosTree1HaveTree2(root1, root2);
-            if (!result)
-                result = HasSubtree(root1.left, root2);
-            if (!result)
-                result = HasSubtree(root1.right, root2);
-        }
-        return result;
-    }
-    public boolean dosTree1HaveTree2(TreeNode root1, TreeNode root2) {
-        if (root2 == null)
-            return true;
-        if (root1 == null)
-            return false;
-        if (root1.val != root2.val)
-            return false;
-        return dosTree1HaveTree2(root1.left, root2.left) && dosTree1HaveTree2(root1.right, root2.right);
-    }
 }

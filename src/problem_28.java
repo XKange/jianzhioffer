7112c78e193cd54d44f6502b605557bf204b7082
xkang-labu <676613599@qq.com>
2018-01-06 12:42:52 +0800

    add 28 code

--- src/problem_28.java
+++ src/problem_28.java
@@ -1,9 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * Created by kangkang on 2017/12/23
+ * 字符串排列
  */
 public class problem_28 {
-    public static int a;
-    public static void main(String[] args) {
-        System.out.print(a);
+    public ArrayList Permutation(String str) {
+        ArrayList res = new ArrayList();
+        if (str != null && str.length() > 0) {
+            PermutationHelper(str.toCharArray(), 0, res);
+            Collections.sort(res);
+        }
+        return res;
+    }
+
+    public void PermutationHelper(char[] cs, int i, ArrayList list) {
+        if (i == cs.length - 1) {
+            String val = String.valueOf(cs);
+            if (!list.contains(val))
+                list.add(val);
+        } else {
+            for (int j = i; j < cs.length; ++j) {
+                swap(cs, i, j);
+                PermutationHelper(cs, i + 1, list);
+                swap(cs, i, j);
+            }
+        }
+    }
+
+    private static void swap(char[] x, int a, int b) {
+        char t = x[a];
+        x[a] = x[b];
+        x[b] = t;
     }
 }
